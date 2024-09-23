997070a200fa8dd0134ee5386ec746039f34e6b8
Nema <kingshousecn@gmail.com>
2024-09-23 12:51:54 +0100

    test1

---
+++ script.js
@@ -0,0 +1,6 @@
+//  git status                 -- checks status of saved files
+//  git add .                  -- add all files into ready for commit 
+//  git commit -m "COMMENT"    -- creates a commit with a comment
+//  git push -u origin master  -- commits to online Repo (MASTER)
+
+// change something
